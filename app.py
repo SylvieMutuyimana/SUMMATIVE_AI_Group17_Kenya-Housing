23610e2cf815b99f8d5b5d3e55e52bc7605a5ee1
Sylvie Mutuyimana <s.mutuyiman@alustudent.com>
2023-04-24 23:24:18 +0200

    app ready

--- app.py
+++ app.py
@@ -29,21 +29,21 @@ def index():
         town = request.form.get('town')
         duration = request.form.get('duration')
         duration_value = request.form.get('duration_value', type=int)
-        
+
         # Collect all the inputs except the town so that we can predict for each other town
         X_num = pd.DataFrame({'Bedrooms': [bedrooms], 'Bathrooms': [bathrooms],'sq_mtrs': [sq_mtrs]})
         X_num = pd.concat([X_num]*len(data['Town'].unique()), ignore_index=True)
         X_cat = pd.DataFrame({'Town': data['Town'].unique()})
         X_cat_encoded = pd.DataFrame(ohe.transform(X_cat).toarray(), columns=ohe.get_feature_names_out(['Town']))
         X = pd.concat([X_num, X_cat_encoded], axis=1)
-        
+
         # Make the predictions for all towns and adjust based on the duration and num values
         price_preds = clf.predict(X)
         if duration == 'months':
             price_preds *= duration_value / 12
         else:
             price_preds *= duration_value / 365
-        
+
         # Add the predicted prices to the data_copy DataFrame for the corresponding Town
         data_copy = data.copy()
         data_copy['Price'] = np.nan
@@ -51,30 +51,16 @@ def index():
         for i, town_name in enumerate(ohe.categories_[0]):
             mask = data_copy['Town'] == town_name
             data_copy.loc[mask, 'Price'] = price_preds[i]
-        
+
         # Get the top 5 highest priced towns and the 5 lowest priced towns
-        lowest_price_towns = data_copy.sort_values('Price').head(5)[['Town', 'Price']]
-        lowest_price_towns = []
-        for _, row in lowest_price_towns.iterrows():
-            town = row['Town']
-            price = row['Price']
-            if not any(t['Town'] == town for t in lowest_price_towns):
-                lowest_price_towns.append({'Town': town, 'Price': price})
-                if len(lowest_price_towns) == 5:
-                    break
-        highest_price_towns = data_copy.sort_values('Price', ascending=False).head(5)[['Town', 'Price']]
-        highest_price_towns = []
-        for _, row in highest_price_towns.iterrows():
-            town = row['Town']
-            price = row['Price']
-            if not any(t['Town'] == town for t in highest_price_towns):
-                highest_price_towns.append({'Town': town, 'Price': price})
-                if len(highest_price_towns) == 5:
-                    break
+        lowest_price_towns = data_copy[['Town', 'Price']].sort_values('Price').drop_duplicates().head(5).to_dict('records')
         len_lowest_price_towns = len(lowest_price_towns)
+        highest_price_towns = data_copy[['Town', 'Price']].sort_values('Price', ascending=False).drop_duplicates().head(5).to_dict('records')
         len_highest_price_towns = len(highest_price_towns)
+
         # Round the final prediction to 2 decimal places
         price_pred = round(data_copy.loc[data_copy['Town'] == town, 'Price'].iloc[0], 2)
+        
         # Render the results table with the additional rows for lowest and highest price towns
         return render_template('index.html', towns=sorted(ohe.categories_[0]), 
                         bedrooms=bedrooms, bathrooms=bathrooms,
